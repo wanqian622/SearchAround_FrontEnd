364031f389710be218b29d23ebf1b32ec6c3d76f
wanqian622 <wanqian622@gmail.com>
2018-04-07 19:31:46 -0700

    show info window

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {withScriptjs,withGoogleMap,GoogleMap,Marker, InfoWindow} from 'react-google-maps';
 
 class AroundMap extends React.Component{
+    state = {
+        isOpen:false,
+    }
+
+    onToggleOpen = ()=>{
+        this.setState((prevState)=>{
+            return {isOpen:!prevState.isOpen}
+        })
+    }
     render(){
         return(
             <GoogleMap
@@ -9,7 +18,9 @@ class AroundMap extends React.Component{
                 defaultCenter={{ lat: -34.397, lng: 150.644 }}
             ><Marker
                     position={{ lat: -34.397, lng: 150.644 }}
-            > </Marker>
+                    onClick={this.onToggleOpen}
+            >{this.state.isOpen ? <InfoWindow onCloseClick={this.onToggleOpen}>
+                    <div>abc</div></InfoWindow> : null}</Marker>
             </GoogleMap>
         )
     }
